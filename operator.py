593e22b280a3c79b4471763a80a2628a7fcd4e68
hananli <hananli@umich.edu>
2017-10-27 15:53:25 -0400

    final commit

---
+++ operator.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python5
+def add(a,b):
+	return a + b
+def subtract(a,b):
+	return a - b
--- rpn.py
+++ rpn.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python5
 
+import operator
+
+ops = {
+	'+': operator.add,
+	'-': operator.subtract,
+}
 def calculate(myarg):
 	stack = list()
 	for token in myarg.split():
-		if token == '+':
+		try:
+			stack.append(int(token))
+		except ValueError:
 			arg2 = stack.pop()
 			arg1 = stack.pop()
-			result = arg1 + arg2
+			function = ops[token]
+			result = function(arg1,arg2)
 			stack.append(result)
-		elif token == '-':
-			arg2 = stack.pop()
-			arg1 = stack.pop()
-			result = arg1 - arg2
-			stack.append(result)
-		else:
-			stack.append(int(token))	
 	print(stack)
 	return stack.pop()
+		
 
 def main():
 	while True:
