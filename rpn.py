a1dd4ed5cb2d4872dfa840785425b8ed9456b4bf
hananli <hananli@umich.edu>
2017-10-27 12:20:56 -0400

    Implemented ADD(+)

--- rpn.py
+++ rpn.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python5
 
 def calculate(myarg):
-	pass
+	stack = list()
+	for token in myarg.split():
+		if token == '+':
+			arg1 = stack.pop()
+			arg2 = stack.pop()
+			result = arg1 + arg2
+			stack.append(result)
+		else:
+			stack.append(int(token))	
+	print[stack]
+	return stack.pop()
 
 def main():
 	while True:
